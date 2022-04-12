3b2fe764cb5de0d8884b1514853731e80cd072e3
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2022-04-12 12:40:20 +0530

    getting accessToken

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,7 @@
 import "./LandingPage.css";
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
 export default function LandingPage() {
   return (
     <div className="landingpage-parent">
@@ -9,6 +11,14 @@ export default function LandingPage() {
 
       <div className="login-holder">
         <h1>Login</h1>
+        {/* <Link to="https://d1v4oztjof20qz.auth.ap-south-1.amazoncognito.com/login?client_id=vf5064o5alo16krvjd4n83d2p&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d1v4oztjof20qz.cloudfront/search">
+          {" "}
+        </Link> */}
+
+        <a href="https://d1v4oztjof20qz.auth.ap-south-1.amazoncognito.com/login?client_id=vf5064o5alo16krvjd4n83d2p&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d1v4oztjof20qz.cloudfront/search">
+          {" "}
+          <button className="login-button">Login with google</button>
+        </a>
       </div>
     </div>
   );
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Search.css";
 import ResultsContainer from "../Components/ResultsContainer";
-import { getBooks } from "../utils/getBooks";
-
-function onChangeHandler(event, setInput) {
-  setInput(event.target.value);
-}
+import getBooks from "../utils/getBooks";
+import onChangeHandler from "../utils/onChangeHandler";
+import { useParams, useSearchParams } from "react-router-dom";
+import axios from "axios";
 
 function Search() {
   const [input, setInput] = useState("");
   const [books, setBooks] = useState(null);
+
+  // const { code } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  let code = searchParams.get("code");
+
+  console.log(code);
+
+  async function getAccessToken() {
+    const params = new URLSearchParams();
+    params.append("client_id", "vf5064o5alo16krvjd4n83d2p");
+    params.append("code", code);
+    params.append("grant_type", "authorization_code");
+    params.append(
+      "redirect_uri",
+      "https://d1v4oztjof20qz.cloudfront.net/search"
+    );
+    console.log(params);
+    // axios.post("/foo", params);
+    try {
+      const response = await axios.get(
+        "https://d1v4oztjof20qz.auth.ap-south-1.amazoncognito.com/oauth2/token",
+        params
+      );
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getAccessToken();
+  }, []);
 
   return (
     <div className="searchpage-parent">
