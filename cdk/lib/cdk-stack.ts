cc227a5a7651d4a723c4efca7be610946b2c9cd9
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2022-05-02 19:36:51 +0530

    cicd pipeline first commit

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,20 @@
-import { Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib";
+import { Stack, StackProps, aws_s3 as s3, SecretValue } from "aws-cdk-lib";
 import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
+import { BuildSpec, Project } from "aws-cdk-lib/aws-codebuild";
+import { Artifact, Pipeline } from "aws-cdk-lib/aws-codepipeline";
+import {
+  CodeBuildAction,
+  GitHubSourceAction,
+  GitHubTrigger,
+} from "aws-cdk-lib/aws-codepipeline-actions";
+import { ManagedPolicy, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -36,5 +49,69 @@ export class CdkStack extends Stack {
         },
       ],
     });
+
+    // new CodePipeline(this, "Pipeline", {
+    //   pipelineName: "wyzrcdkpipeline",
+    //   synth: new ShellStep("synth", {
+    //     input: CodePipelineSource.gitHub(
+    //       "AdityaNair102001/wyzr-assessment",
+    //       "main"
+    //     ),
+    //     commands: ["npm ci", "npm run build", "npx cdk synth"],
+    //   }),
+    // });
+
+    const pipeline = new Pipeline(this, "FrontendPipeline", {
+      pipelineName: "deploy-angular-application",
+    });
+
+    // add Stages
+
+    const sourceStage = pipeline.addStage({
+      stageName: "Source",
+    });
+
+    const buildStage = pipeline.addStage({
+      stageName: "Build",
+      placement: {
+        justAfter: sourceStage,
+      },
+    });
+
+    const sourceOutput = new Artifact();
+    const sourceAction = new GitHubSourceAction({
+      actionName: "GitHub",
+      owner: "AdityaNair102001",
+      repo: "wyzr-assessment",
+      oauthToken: SecretValue.secretsManager(
+        "arn:aws:secretsmanager:ap-south-1:014651819218:secret:github-token-VajTwj"
+      ),
+      output: sourceOutput,
+      branch: "main",
+      trigger: GitHubTrigger.POLL, // default: 'WEBHOOK', 'NONE' is also possible for no Source trigger
+    });
+
+    sourceStage.addAction(sourceAction);
+
+    const role = new Role(this, "CodeBuildRole", {
+      assumedBy: new ServicePrincipal("codebuild.amazonaws.com"),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+        ManagedPolicy.fromAwsManagedPolicyName("CloudFrontFullAccess"),
+      ],
+    });
+
+    const codebuild = new Project(this, "WyzrCodeBuild", {
+      role,
+      buildSpec: BuildSpec.fromSourceFilename("buildspec.yml"),
+    });
+
+    const buildAction = new CodeBuildAction({
+      actionName: "Build",
+      input: sourceOutput,
+      project: codebuild,
+    });
+
+    buildStage.addAction(buildAction);
   }
 }
